c6e2b566f7854b50fac9deb0881fedae88c9a271
Witek <witold.zzak@gmail.com>
2025-08-24 16:54:34 +0200

    Added functions to buttons

--- ui.py
+++ ui.py
@@ -29,28 +29,37 @@ def update_value():
 
     root.after(1000, update_value)
 
+def dashboard():
+    global bg_photo
+    canvas.itemconfig("dashboard", state='normal')
+    bg_image = Image.open("Dashboard.png")
+    bg_photo = ImageTk.PhotoImage(bg_image)
+    canvas.itemconfig(bg_item, image=bg_photo)
+
 def reports():
-    pass
+    global bg_photo
+    canvas.itemconfig("dashboard", state='hidden')
+    bg_image = Image.open("Reports.png")
+    bg_photo = ImageTk.PhotoImage(bg_image)
+    canvas.itemconfig(bg_item, image=bg_photo)
 
 root = ctk.CTk()
-
 root.title("test")
-bg_image = Image.open("Dashboard.png")
 
+bg_image = Image.open("Dashboard.png")
 bg_photo = ImageTk.PhotoImage(bg_image)
 canvas = ctk.CTkCanvas(root, width=bg_image.width, height=bg_image.height)
+bg_item = canvas.create_image(0, 0, anchor="nw", image=bg_photo)
 canvas.pack()
 
-canvas.create_image(0, 0, anchor="nw", image=bg_photo)
-
-dashboard_button = ctk.CTkButton(root, text="Dashboard", fg_color="#21164A", width=250, height=50)
+dashboard_button = ctk.CTkButton(root, text="Dashboard", fg_color="#21164A", width=250, height=50, command=dashboard)
 reports_button = ctk.CTkButton(root, text="Reports", fg_color="#21164A", width=250, height=50, command=reports)
 
 canvas.create_window(130,400, window=dashboard_button)
 canvas.create_window(130,450, window=reports_button)
-total_item = canvas.create_text(485, 195, text="", font=("Arial", 15), fill="white")
 
-apps_item = canvas.create_text(840, 195, text="", font=("Arial", 15), fill="white")
+total_item = canvas.create_text(485, 195, text="", font=("Arial", 15), fill="white",tags='dashboard')
+apps_item = canvas.create_text(840, 195, text="", font=("Arial", 15), fill="white",tags='dashboard')
 
 update_value()
 
